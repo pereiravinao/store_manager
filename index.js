b95c450c6ea4325884a4130775046fe8929991c7
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-25 17:47:55 -0300

    Adicionando BodyParser

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const createProduct = require('./controllers/createProduct');
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.post('/products', createProduct);
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
