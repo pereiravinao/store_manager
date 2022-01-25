292281f033dda651500b6ecc3ee11841b045a803
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-25 19:02:07 -0300

    Corrigindo nome do BD

--- models/getAllProducts.js
+++ models/getAllProducts.js
@@ -2,11 +2,10 @@ const connection = require('./connection');
 
 const getAllProducts = async () => {
   const [products] = await connection.execute(
-      'SELECT name FROM StorageManager.products;',
+      'SELECT name FROM StoreManager.products;',
   );
   return products;
 };
-
 module.exports = {
   getAllProducts,
 };
