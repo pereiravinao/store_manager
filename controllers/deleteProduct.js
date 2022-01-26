d422376286d19f257a26210b22e935b65812eb4e
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-26 20:45:08 -0300

    Adiciona camada de controller para deletar produto

---
+++ controllers/deleteProduct.js
@@ -0,0 +1,10 @@
+const deleteP = require('../services/deleteProduct');
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  const deleted = await deleteP(id);
+  if (deleted.code) return res.status(deleted.code).json({ message: deleted.message });
+  res.status(200).json(deleted);
+};
+
+module.exports = deleteProduct;
