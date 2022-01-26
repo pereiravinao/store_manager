2a60827ca26bfaca06e7f47378170b57465d0c77
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-26 19:44:40 -0300

    Ajustado o valor do Id de fixo para dinamico

--- controllers/createProduct.js
+++ controllers/createProduct.js
@@ -1,10 +1,12 @@
 const isValid = require('../services/Validations');
+const saveInDB = require('../models/SaveProducts');
 
 const createProduct = async (req, res) => {
   const { name, quantity } = req.body;
-  const { code, message } = await isValid(name, quantity);
-  if (message) return res.status(code).json({ message }); 
-  res.status(201).json({ name, quantity, id: 1 });
+  const { code, message, id } = await isValid(name, quantity);
+  if (message) return res.status(code).json({ message });
+  saveInDB(name, quantity);
+  res.status(201).json({ name, quantity, id });
 };
 
 module.exports = createProduct;
