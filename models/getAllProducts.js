d7d1ac0dc81b1d4eae9596ff7279fdb5e733692c
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-26 19:43:59 -0300

    adicionada a funcao para buscar pelo id dentro do Bd

--- models/getAllProducts.js
+++ models/getAllProducts.js
@@ -6,6 +6,21 @@ const getAllProducts = async () => {
   );
   return products;
 };
+
+const findByIdModel = async (id) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const [product] = await connection.execute(query, [id]);
+  if (product.length === 0) return null;
+
+  const productSelected = {
+      id: product[0].id,
+      name: product[0].name,
+      quantity: product[0].quantity,
+  };
+ return productSelected;
+};
+
 module.exports = {
   getAllProducts,
+  findByIdModel,
 };
