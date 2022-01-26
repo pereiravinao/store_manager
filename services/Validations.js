afedc7cfc2c2d84d4243aa4d8d8b33298e3dbc7b
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-26 17:01:59 -0300

    Criando validacoes

--- services/Validations.js
+++ services/Validations.js
@@ -1,17 +1,32 @@
 const { getAllProducts } = require('../models/getAllProducts');
 
-const isRequired = '\"name\" is required';
+const isRequired = '"name" is required';
+const isQtdRequired = '"quantity" is required';
 const isNotLength = '"name" length must be at least 5 characters long';
 const productExists = 'Product already exists';
+const isNotNumber = '"quantity" must be a number larger than or equal to 1';
 
 const isValidName = async (name) => {
   const products = await getAllProducts();
-  const product = products.some((p) => p !== name);
+  const product = products.find((p) => p.name === name);
   if (!name) return { code: 400, message: isRequired };
   if (name.length < 5) return { code: 422, message: isNotLength };
   if (product) return { code: 409, message: productExists };
-  
   return {};
 };
 
-module.exports = isValidName;
+const isValidQtd = (quantity) => {
+  if (!quantity && quantity !== 0) return { code: 400, message: isQtdRequired };
+  if (typeof quantity !== 'number' || quantity < 1) return { code: 422, message: isNotNumber };
+  return {};
+};
+
+const isValid = async (name, quantity) => {
+  const validaName = await isValidName(name);
+  const validaQtd = isValidQtd(quantity);
+  if (validaQtd.code) return isValidQtd(quantity);
+  if (validaName.code) return isValidName(name);
+  return {};
+};
+
+module.exports = isValid;
