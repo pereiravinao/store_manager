65b30b5933c8b0210400e15be35ea6cd27939801
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-26 19:59:08 -0300

    Refatorando codigo com funcao do service

--- controllers/findProductById.js
+++ controllers/findProductById.js
@@ -1,9 +1,9 @@
-const { findByIdModel } = require('../models/getAllProducts');
+const findById = require('../services/findById');
 
 const findId = async (req, res) => {
   const { id } = req.params;
-  const product = await findByIdModel(id);
-  if (!product) return res.status(404).json({ message: 'Product not found' });
+  const product = await findById(id);
+  if (product.code) return res.status(product.code).json({ message: product.message });
   res.status(200).json(product);
 };
 
