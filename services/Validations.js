cc7fcd58eadd30d53188eb8c79a343f9a44706cb
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 19:39:15 -0300

    Removendo gerador de ID manual

--- services/Validations.js
+++ services/Validations.js
@@ -23,13 +23,11 @@ const isValidQtd = (quantity) => {
 };
 
 const isValid = async (name, quantity) => {
-  const products = await getAllProducts();
-  const id = Math.max(...products.map((e) => e.id)) + 1 || 1;
   const validaName = await isValidName(name);
   const validaQtd = isValidQtd(quantity);
   if (validaQtd.code) return isValidQtd(quantity);
   if (validaName.code) return isValidName(name);
-  return { id };
+  return { };
 };
 
 const isValidProductId = (productId) => {
