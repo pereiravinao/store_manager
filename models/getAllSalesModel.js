07319bdaa733864e1b99bdb2daa9b5a8816eb7fb
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 16:29:30 -0300

    Removendo comentarios

--- models/getAllSalesModel.js
+++ models/getAllSalesModel.js
@@ -29,11 +29,6 @@ const findByIdModel = async (id) => {
   const [product] = await connection.execute(query, [id]);
   if (product.length === 0) return null;
 
-  // const productSelected = {
-  //     id: product[0].id,
-  //     name: product[0].name,
-  //     quantity: product[0].quantity,
-  // };
  return product;
 };
 
