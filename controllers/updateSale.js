c80f8e7276719f860b558e15fd786708106de6eb
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 18:09:42 -0300

    Camada Controller para atualizar venda

--- controllers/updateSale.js
+++ controllers/updateSale.js
@@ -3,9 +3,9 @@ const updateSale = require('../services/updateSale');
 const updateSales = async (req, res) => {
   const { id } = req.params;
   const arrayRecived = req.body;
-  const saleUpdated = updateSale(id, arrayRecived);
-  if (saleUpdated[0].code) {
-    return res.status(saleUpdated[0].code).json({ message: saleUpdated[0].message }); 
+  const saleUpdated = await updateSale(id, arrayRecived);
+  if (saleUpdated.code) {
+    return res.status(saleUpdated.code).json({ message: saleUpdated.message }); 
   }
   res.status(200).json(saleUpdated);
 };
