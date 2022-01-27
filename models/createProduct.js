bf47e79ad1e2e53d9eae11950b81d7bfd8130092
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 19:43:22 -0300

    Renomeando arquivo

---
+++ models/createProduct.js
@@ -0,0 +1,11 @@
+const connection = require('./connection');
+
+const SaveProducts = async (name, quantity) => {
+    const [{insertId}] = await connection.execute(
+    'INSERT INTO StoreManager.products (name, quantity) VALUES (?,?)',
+    [name, quantity],
+);
+    return insertId; 
+}
+
+module.exports = SaveProducts;
