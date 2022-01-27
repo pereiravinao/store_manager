0f94ea4fcb6cec83c9c270e8e47eb7ad1ddee5cd
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 18:20:18 -0300

    Alternando forma de gerar ID - TESTE

--- models/createSaleModel.js
+++ models/createSaleModel.js
@@ -5,8 +5,8 @@ const queryInsert = `INSERT INTO StoreManager.sales_products
 
 // Logica do forEach retirada do PR do Rodrigo Marchi, não estava enchergando a ncessidade de add primeiro no sales e dps no sales_products
 const createSaleModel = async (sale) => {
-  const [sales] = await connection.query('SELECT MAX(id) FROM StoreManager.sales');
-  const id = sales[0]['MAX(id)'] + 1 || 1;
+  const [sales] = await connection.query('SELECT * FROM StoreManager.sales');
+  const id = sales.length + 1 || 1;
   await connection.query('INSERT INTO StoreManager.sales (id) VALUE (?)', [id]);
   sale.forEach(async (product) => {
     await connection.query(queryInsert, [id, product.product_id, product.quantity]);
