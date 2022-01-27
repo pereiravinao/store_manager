0e54318b629df3a2ccb7a5debb5181065b341c50
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 18:10:27 -0300

    Logica com validacao para atualizacao

--- services/updateSale.js
+++ services/updateSale.js
@@ -1,17 +1,13 @@
 const updateSaleModel = require('../models/updateSaleModel');
-const { findByIdModel } = require('../models/getAllSalesModel');
 const { isValidSale } = require('./Validations');
 
 const updateProduct = async (id, arr) => {
-  const { product_id: productId, quantity } = arr;
-  const validSale = arr.map((sale) => {
-    const validaVenda = isValidSale(sale.product_id, sale.quantity);
-    if (validaVenda.code) { return { code: validaVenda.code, message: validaVenda.message }; }
-    return {};
-  });
-  if (validSale[0].code) return validSale;
+  const { product_id: productId, quantity } = arr[0];
+  const validaVenda = isValidSale(productId, quantity);
+  if (validaVenda.code) { return { code: validaVenda.code, message: validaVenda.message }; }
   await updateSaleModel(productId, quantity, id);
-  const newProduct = await findByIdModel(productId);
+  const newProduct = { saleId: Number(id), itemUpdated: arr };
+  console.log(newProduct);
   return newProduct;
 };
 
