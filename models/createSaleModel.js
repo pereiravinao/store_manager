4274e22a0063636d249b3dfd3af634cbac562b53
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 18:29:05 -0300

    Outra tentativa no gerador de ID

--- models/createSaleModel.js
+++ models/createSaleModel.js
@@ -5,13 +5,15 @@ const queryInsert = `INSERT INTO StoreManager.sales_products
 
 // Logica do forEach retirada do PR do Rodrigo Marchi, não estava enchergando a ncessidade de add primeiro no sales e dps no sales_products
 const createSaleModel = async (sale) => {
-  const [sales] = await connection.query('SELECT * FROM StoreManager.sales');
-  const id = sales.length + 1 || 1;
-  await connection.query('INSERT INTO StoreManager.sales (id) VALUE (?)', [id]);
+  const [{ insertId }] = await connection.execute('INSERT INTO StoreManager.sales () VALUES ()');
+  console.log(insertId);
+  // const [sales] = await connection.query('SELECT * FROM StoreManager.sales');
+  // const id = sales.length + 1 || 1;
+  // await connection.query('INSERT INTO StoreManager.sales (id) VALUE (?)', [id]);
   sale.forEach(async (product) => {
-    await connection.query(queryInsert, [id, product.product_id, product.quantity]);
+    await connection.query(queryInsert, [insertId, product.product_id, product.quantity]);
   });
-  return { id, itemsSold: sale };
+  return { id: insertId, itemsSold: sale };
 };
 
 module.exports = createSaleModel;
