de1597a33ab3e01ac7993091d8b3caa22f87b2e2
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 19:43:49 -0300

    Refatorando nomes da funcao para salvar no DB

--- controllers/createProduct.js
+++ controllers/createProduct.js
@@ -1,11 +1,11 @@
 const { isValid } = require('../services/Validations');
-const saveInDB = require('../models/SaveProducts');
+const createProductModel = require('../models/createProduct');
 
 const createProduct = async (req, res) => {
   const { name, quantity } = req.body;
   const { code, message } = await isValid(name, quantity);
   if (message) return res.status(code).json({ message });
-  const id = await saveInDB(name, quantity);
+  const id = await createProductModel(name, quantity);
   res.status(201).json({ name, quantity, id });
 };
 
