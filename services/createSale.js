41e82135243e5de441762b96b7e91659a2fe0ca3
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 16:25:52 -0300

    Removendo Console Log

--- services/createSale.js
+++ services/createSale.js
@@ -5,12 +5,10 @@ const createSale = async (arraySales) => {
   const arr = arraySales.map((sale) => {
       const productId = sale.product_id;
       const validaVenda = isValidSale(productId, sale.quantity);
-      console.log('valida: ', validaVenda);
       if (validaVenda.code) { return { code: validaVenda.code, message: validaVenda.message }; }
       return {};
     });
     if (arr[0].code) return arr;
-    console.log('service: ', arr);
  
   const saleCreated = await createSaleModel(arraySales);
   return [saleCreated];
