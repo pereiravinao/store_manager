1154b105035db3b02e38556ef810dfd25d9dc40c
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-27 16:24:17 -0300

    Model para listar todas as vendas

--- models/getAllSalesModel.js
+++ models/getAllSalesModel.js
@@ -2,22 +2,39 @@ const connection = require('./connection');
 
 const getAllSales = async () => {
   const [products] = await connection.execute(
-      'SELECT * FROM StoreManager.sales_products;',
+      `SELECT 
+        sale_id AS saleId,
+        quantity,
+        product_id,
+        date
+      FROM
+          StoreManager.sales_products AS sp
+              INNER JOIN
+          StoreManager.sales AS sl ON sl.id = sp.sale_id`,
   );
   return products;
 };
 
 const findByIdModel = async (id) => {
-  const query = 'SELECT * FROM StoreManager.sales_products WHERE id = ?';
+  const query = `SELECT 
+                  quantity,
+                  product_id,
+                  date
+                FROM
+                    StoreManager.sales_products AS sp
+                        INNER JOIN
+                    StoreManager.sales AS sl ON sl.id = sp.sale_id
+                    WHERE id = ?`;
+
   const [product] = await connection.execute(query, [id]);
   if (product.length === 0) return null;
 
-  const productSelected = {
-      id: product[0].id,
-      name: product[0].name,
-      quantity: product[0].quantity,
-  };
- return productSelected;
+  // const productSelected = {
+  //     id: product[0].id,
+  //     name: product[0].name,
+  //     quantity: product[0].quantity,
+  // };
+ return product;
 };
 
 module.exports = {
