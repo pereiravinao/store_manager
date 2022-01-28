90d55b309c159abce9fe4b89ca29fb7626848548
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-28 17:02:14 -0300

    Criando testes para validacao - Parcial

--- test/unit/services.js
+++ test/unit/services.js
@@ -6,13 +6,14 @@ const connection = require('../../models/connection');
 const { isValid, isValidSale, isValidProductId, isValidName, isValidQtd } = require('../../services/Validations');
 
 const product = {
-  noName: { quantity: 2},
-  invalidName: { name: "abc", quantity: 2},
-  noQuantity: { name: "Algum produto"},
-  invalidQuantity: { name: "Outro Produto", quantity: "letra"},
-  correct: { name: 'Primeiro Produto', quantity: 4 },
-  returnCreate: {id: 1, name: 'Primeiro Produto', quantity: 4 },
-  returnUptade: {id: 1, name: 'Produto Atualizado', quantity: 400 }
+  returnCreate: {id: 1, name: 'Primeiro Produto', quantity: 4 }
+}
+
+const sales = { 
+  createSale: { productId: 1, quantity: 3},
+  returnSaleCreate: {id: 1, product_id: 1, quantity: 4},
+  allSales: { saleId: 1, quantity: 3, product_id: 1, date: 20220128},
+  saleById: { quantity: 1, product_id: 1, date: 20220128}
 }
 
 describe('Verificando as validaçãoes', async () => {
@@ -26,14 +27,14 @@ describe('Verificando as validaçãoes', async () => {
 
     it('Retorna erro 400 se NAME não existir', async () => {
       const result = await isValid();
-      expect(result.code).to.deep.equal(400)
+      expect(result.code).to.be.equal(400)
     });
     it('Retorna erro 422 se NAME menor que 5 letras', async () => {
       const result = await isValidName('abcd');
       expect(result.code).to.be.equal(422)
     });
     it('Retorna erro 400 se QUANTITY não existir', async () => {
-      const result = await isValidQtd();
+      const result = await isValid();
       expect(result.code).to.deep.equal(400)
     });
     it('Retorna erro 422 se QUANTITY for diferente de um numero', async () => {
@@ -44,7 +45,7 @@ describe('Verificando as validaçãoes', async () => {
 
   describe('Validando infos para cadastrar nova venda:', async () => {
     before(async () => {
-      sinon.stub(connection, 'execute').resolves([[product.returnCreate]]);
+      sinon.stub(connection, 'execute').resolves([[sales.createSale]]);
     });
     after(async () => {
       connection.execute.restore();
@@ -54,16 +55,12 @@ describe('Verificando as validaçãoes', async () => {
       const result = await isValidProductId();
       expect(result.code).to.be.equal(400)
     });
-    it('Retorna erro 422 se NAME menor que 5 letras', async () => {
-      const result = await isValidSale('abcd');
-      expect(result.code).to.be.equal(422)
-    });
     it('Retorna erro 400 se QUANTITY não existir', async () => {
-      const result = await isValidQtd();
+      const result = await isValidSale();
       expect(result.code).to.deep.equal(400)
     });
-    it('Retorna erro 422 se QUANTITY for diferente de um numero', async () => {
-      const result = await isValidQtd('abcd');
+    it('Retorna erro 422 se QUANTITY for menor ou igual a zero', async () => {
+      const result = await isValidQtd(0);
       expect(result.code).to.be.equal(422)
     });
   });
