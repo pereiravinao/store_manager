d68dafd8795226620d0bceee2a81554d1b678c91
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-28 15:48:03 -0300

    Criado testes na camada models

--- test/unit/models.js
+++ test/unit/models.js
@@ -5,7 +5,10 @@ const connection = require('../../models/connection');
 const createProductModel = require('../../models/createProduct');
 const UpdateProductModel = require('../../models/updateProductsModel');
 const deleteProductModel = require('../../models/deleteProductModel');
+const updateSaleModel = require('../../models/updateSaleModel');
 const {getAllProducts, findByIdModel} = require('../../models/getAllProducts');
+const { createSaleModel, createNewSale } = require('../../models/createSaleModel');
+const { getAllSales, findByIdModel: findByIdModelSale } = require('../../models/getAllSalesModel');
 
 
 
@@ -17,6 +20,13 @@ const product = {
   correct: { name: 'Primeiro Produto', quantity: 4 },
   returnCreate: {id: 1, name: 'Primeiro Produto', quantity: 4 },
   returnUptade: {id: 1, name: 'Produto Atualizado', quantity: 400 }
+}
+
+const sales = { 
+  createSale: { productId: 1, quantity: 3},
+  returnSaleCreate: {id: 1, product_id: 1, quantity: 4},
+  allSales: { saleId: 1, quantity: 3, product_id: 1, date: 20220128},
+  saleById: { quantity: 1, product_id: 1, date: 20220128}
 }
 
 describe('Insere produto no BD', () => {
@@ -120,3 +130,108 @@ describe('Deletar Produto no BD', () => {
     expect(item).to.be.empty;
   });
 });
+
+describe('Criar nova Venda no BD', () => {
+  describe('Adiciona venda no Banco de Dados Sales', async () => { 
+    before(async () => {
+      sinon.stub(connection, 'query').resolves([{ insertId: 1 }]);
+    });
+    after(async () => {
+      connection.query.restore();
+    });
+    it('verifica se retorna um array vazio', async () => { 
+      const item = await createNewSale();
+      expect(item).to.be.equal(1);
+    });
+  });
+
+  describe('Adiciona venda no Banco de Dados Product Sales', async () => { 
+    before(async () => {
+      sinon.stub(connection, 'query').resolves([sales.returnSaleCreate]);
+    });
+    after(async () => {
+      connection.query.restore();
+    });
+    it('verifica se retorna um objeto', async () => {
+      const item = await createSaleModel([sales.createSale]);
+      expect(item).to.be.an('object');
+    });
+    it('o objeto não esta vazio', async () => {
+      const item = await createSaleModel([sales.createSale]);
+      expect(item).to.not.be.empty;
+    });
+    it('verifica se o objeto retornado possui as chaves id e itemsSold', async () => {
+      const item = await createSaleModel([sales.createSale]);
+      expect(item).to.include.all.keys('id', 'itemsSold');
+    });
+    });
+  });
+
+  describe('Busca vendas no BD', () => {
+    describe('retorna todas as vendas listadas no BD', async () => { 
+      before(async () => {
+        sinon.stub(connection, 'execute').resolves([sales.allSales]);
+      });
+      after(async () => {
+        connection.execute.restore();
+      });
+      it('verifica se retorna um objeto', async () => {
+        const item = await getAllSales();
+        expect(item).to.be.an('object');
+      });
+      it('o objeto não esta vazio', async () => {
+        const item = await getAllSales();
+        expect(item).to.not.be.empty;
+      });
+      it('verifica se o objeto retornado possui as chaves saleId, date, quantity e product_id', async () => {
+        const item = await getAllSales();
+        expect(item).to.include.all.keys('saleId', 'date', 'quantity', 'product_id');
+      });
+    });
+
+    describe('retorna a venda do ID selecionado', async () => { 
+      before(async () => {
+        sinon.stub(connection, 'execute').resolves([[sales.saleById]]);
+      });
+      after(async () => {
+        connection.execute.restore();
+      });
+      it('verifica se retorna um array', async () => {
+        const item = await findByIdModelSale(1);
+        expect(item).to.be.an('array');
+      });
+      it('o objeto não esta vazio', async () => {
+        const item = await findByIdModelSale(1);
+        expect(item).to.not.be.empty;
+      });
+      it('verifica se o objeto retornado possui as chaves date, quantity e product_id', async () => {
+        const item = await findByIdModelSale(1);
+        expect(item[0]).to.include.all.keys('date', 'quantity', 'product_id');
+      });
+    });
+    describe('Quando ID não existe', async () => { 
+      before(async () => {
+        sinon.stub(connection, 'execute').resolves([[]]);
+      });
+      after(async () => {
+        connection.execute.restore();
+      });
+      it('retornar valor null', async () => {
+        const item = await findByIdModelSale(3);
+        expect(item).to.be.null;
+      });
+    });
+  });
+
+  describe('Atualizar Venda no BD', () => {
+    before(async () => {
+      sinon.stub(connection, 'execute').resolves();
+    });
+    after(async () => {
+      connection.execute.restore();
+    });
+    it('verifica se retorna valor undefined', async () => { 
+      const item = await updateSaleModel(1, 3, 1);
+      expect(item).to.be.undefined;
+    });
+  });
