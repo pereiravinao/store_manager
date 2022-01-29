698927d4fb19877117bfa751fbdc1304ce5edaaf
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-01-29 12:23:54 -0300

    REQUISITO 12

--- services/Estoque.js
+++ services/Estoque.js
@@ -3,14 +3,17 @@ const ProductsModel = require('../models/ProductsModel');
 const updateEstoque = async (quantity, productId, deleteSale) => {
   const getQuantity = await ProductsModel.getQuantity(productId);
   if (getQuantity.length > 0) {
+    if (getQuantity[0].quantity - quantity < 0) { 
+      return { code: 422, message: 'Such amount is not permitted to sell' }; 
+      }
     if (deleteSale) {
       const newQuantity = getQuantity[0].quantity + quantity;
-      console.log('delete: ', newQuantity);
       await ProductsModel.updateQuantity(newQuantity, productId);
-      } else {
+      return {};
+      } 
     const newQuantity = getQuantity[0].quantity - quantity;
         await ProductsModel.updateQuantity(newQuantity, productId);
-      } 
+        return {};
 }
   };
 
--- services/createSale.js
+++ services/createSale.js
@@ -6,7 +6,12 @@ const createSale = async (arraySales) => {
   const arr = await Promise.all(arraySales.map(async (sale) => {
       const validaVenda = await isValidSale(sale.product_id, sale.quantity);
       if (validaVenda.code) { return { code: validaVenda.code, message: validaVenda.message }; }
-      await Estoque.updateEstoque(sale.quantity, sale.product_id);
+      const updateEstoque = await Estoque.updateEstoque(sale.quantity, sale.product_id);
+      if (updateEstoque.code) {
+    console.log('isNotFunction:', updateEstoque);
+
+        return { code: updateEstoque.code, message: updateEstoque.message }; 
+}
       return {};
     }));
     if (arr[0].code) return arr;
